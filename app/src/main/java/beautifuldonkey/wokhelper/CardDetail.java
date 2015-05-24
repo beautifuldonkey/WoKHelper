0dfc56c1750a359a8716b610940647f6f261cd98
beautifuldonkey <beautifuldonkey949@gmail.com>
2015-05-24 14:21:02 -0700

    adding defenses to card layout

--- app/src/main/java/beautifuldonkey/wokhelper/CardDetail.java
+++ app/src/main/java/beautifuldonkey/wokhelper/CardDetail.java
@@ -14,6 +14,19 @@ public class CardDetail extends ActionBarActivity {
 
     public static final String CARD_ID = "card_id";
 
+    TextView unitName;
+    TextView unitTitle;
+    TextView defOne;
+    TextView defTwo;
+    TextView defThree;
+    TextView defFour;
+    TextView defFive;
+    TextView defSix;
+    TextView defSeven;
+    TextView defEight;
+    TextView defNine;
+    TextView defTen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -22,9 +35,46 @@ public class CardDetail extends ActionBarActivity {
 
         //int cardId = getIntent().getIntExtra(CardDetail.CARD_ID, 0);
         Card card = getIntent().getParcelableExtra("CARD");
+        displayCard(card);
+    }
 
-        TextView dispUnitName = (TextView) findViewById(R.id.unitName);
-        dispUnitName.setText("id:"+card.getId()+" name:"+card.getName()+" title:"+card.getTitle());
+    protected void displayCard(Card card){
 
+        unitName = (TextView) findViewById(R.id.unitName);
+        unitName.setText(card.getName());
+
+
+        unitTitle = (TextView) findViewById(R.id.unitTitle);
+        unitTitle.setText(card.getTitle());
+
+        defOne = (TextView) findViewById(R.id.defOne);
+        defOne.setText(card.getDefOne());
+
+        defTwo = (TextView) findViewById(R.id.defTwo);
+        defTwo.setText(card.getDefTwo());
+
+        defThree = (TextView) findViewById(R.id.defThree);
+        defThree.setText(card.getDefThree());
+
+        defFour = (TextView) findViewById(R.id.defFour);
+        defFour.setText(card.getDefFour());
+
+        defFive = (TextView) findViewById(R.id.defFive);
+        defFive.setText(card.getDefFive());
+
+        defSix = (TextView) findViewById(R.id.defSix);
+        defSix.setText(card.getDefSix());
+
+        defSeven = (TextView) findViewById(R.id.defSeven);
+        defSeven.setText(card.getDefSeven());
+
+        defEight = (TextView) findViewById(R.id.defEight);
+        defEight.setText(card.getDefEight());
+
+        defNine = (TextView) findViewById(R.id.defNine);
+        defNine.setText(card.getDefNine());
+
+        defTen = (TextView) findViewById(R.id.defTen);
+        defTen.setText(card.getDefTen());
     }
 }
