bf8d907123ff44ad5415f74dfd3e46a309a6170e
beautifuldonkey <beautifuldonkey949@gmail.com>
2015-05-24 20:44:02 -0700

    updating ability item layout adding array adapter for abilities

--- app/src/main/java/beautifuldonkey/wokhelper/CardDetail.java
+++ app/src/main/java/beautifuldonkey/wokhelper/CardDetail.java
@@ -1,12 +1,23 @@
 package beautifuldonkey.wokhelper;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
+import beautifuldonkey.wokhelper.Data.Ability;
+import beautifuldonkey.wokhelper.Data.AbilityList;
 import beautifuldonkey.wokhelper.Data.Card;
+import beautifuldonkey.wokhelper.Data.House;
 
 /**
  * Created by Jed on 5/19/2015.
@@ -38,7 +49,7 @@ public class CardDetail extends ActionBarActivity {
         setContentView(R.layout.unit_details);
         Context context = getApplicationContext();
 
-        //int cardId = getIntent().getIntExtra(CardDetail.CARD_ID, 0);
+        List<Ability> abilities = AbilityList.getAbilities();
         Card card = getIntent().getParcelableExtra("CARD");
         displayCard(card);
     }
@@ -93,5 +104,33 @@ public class CardDetail extends ActionBarActivity {
 
         defTen = (TextView) findViewById(R.id.defTen);
         defTen.setText(card.getDefTen());
+    }
+}
+
+class abilityArrayAdapter extends ArrayAdapter<Ability> {
+
+    Context context;
+    List<Ability> objects;
+
+    public abilityArrayAdapter(Context context, int resource, List<Ability> objects) {
+        super(context, resource, objects);
+        this.context = context;
+        this.objects = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Ability ability = objects.get(position);
+
+        LayoutInflater inflater =  (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        View view = inflater.inflate(R.layout.ability_item, null);
+
+        TextView tvHouseName = (TextView)view.findViewById(R.id.houseName);
+        tvHouseName.setText(ability.getName());
+
+
+        return view;
     }
 }
