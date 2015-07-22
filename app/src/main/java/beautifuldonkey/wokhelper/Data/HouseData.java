5161994bb7f664cb26301940edea3c35559c94c4
beautifuldonkey <beautifuldonkey9>
2015-07-22 14:28:43 -0700

    HouseData.java -adding motivations to static house data

--- app/src/main/java/beautifuldonkey/wokhelper/Data/HouseData.java
+++ app/src/main/java/beautifuldonkey/wokhelper/Data/HouseData.java
@@ -13,10 +13,10 @@ public class HouseData {
     public static List<House> getHouseList() {return houses;}
 
     static {
-        houses.add(new House(1,"Hadross", "Hadross short summary", "looong description"));
-        houses.add(new House(2,"Teknes", "Teknes short summary", " description"));
-        houses.add(new House(3,"Nasier", "Nasier short summary", " description"));
-        houses.add(new House(4,"Goritsi", "Goritsi short summary", " description"));
-        houses.add(new House(5,"ShaelHan", "ShaelHan short summary", " description"));
+        houses.add(new House(1,"Hadross", "Hadross short summary", "looong description", "Greed,Vengeance"));
+        houses.add(new House(2,"Teknes", "Teknes short summary", "description", "Greed,Treachery"));
+        houses.add(new House(3,"Nasier", "Nasier short summary", "description", "Duty,Vengeance"));
+        houses.add(new House(4,"Goritsi", "Goritsi short summary", "description", "Intrigue,Treachery"));
+        houses.add(new House(5,"ShaelHan", "ShaelHan short summary", "description", "Duty,Intrigue"));
     }
 }
