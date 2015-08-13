f3404aa19fdedc995bcdc9d26903f849f38ed991
beautifuldonkey <beautifuldonkey949@gmail.com>
2015-08-12 21:54:01 -0700

    DeckBuilder.java -adding other Teknes units

--- app/src/main/java/beautifuldonkey/wokhelper/Data/DeckBuilder.java
+++ app/src/main/java/beautifuldonkey/wokhelper/Data/DeckBuilder.java
@@ -55,18 +55,36 @@ public class DeckBuilder{
     // ,wounds, defense, attack, movement, def1-10, abilities )
     static {
 
-        teknesDeck.add(new Card("Teknes", "Defender Lineman", "Citizen", "Infantry", 1,30,2,"B"
-                ,1,1,5,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","X","SONIC","BOLT","BOLT","SKULL","1,9,51"));
         teknesDeck.add(new Card("Teknes", "Union Boss", "Union Member", "Leader", 1,40,3,"C"
                 ,3,1,6,5,"DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","SONIC","BOLT","BOLT","BOLT","SKULL","7,47,48,70,71,72"));
         teknesDeck.add(new Card("Teknes", "Union Worker", "Union Member", "Infantry", 1,30,2,"C"
                 ,2,1,4,5,"DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","BOLT","BOLT","BOLT","BOLT","SKULL","5,40,41"));
+        teknesDeck.add(new Card("Teknes", "The Ironward", "Union Member", "Leader Character", 2,40,3,"C"
+                ,2,2,6,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","SONIC","SONIC","BOLT","BOLT","SKULL","")); //need to add skills
         teknesDeck.add(new Card("Teknes", "The Zaalak", null, "Specialist", 1,50,2,"C"
                 ,1,2,4,8,"SONIC","SONIC","DOUGHNUT","DOUGHNUT","DOUGHNUT","BOLT","BOLT","BOLT","SKULL","SKULL","6,42,43,44"));
         teknesDeck.add(new Card("Teknes", "Galvanic Defender", null, "Specialist", 1,40,3,"A"
                 ,3,1,6,7,"STAR","STAR","STAR","DOUGHNUT","DOUGHNUT","BOLT","BOLT","BOLT","BOLT","SKULL","2,3,4,45,46"));
+        teknesDeck.add(new Card("Teknes", "C.A.G.E.", null, "Specialist", 2,50,4,"B"
+                ,3,2,6,6,"STAR","STAR","DOUGHNUT","DOUGHNUT","DOUGHNUT","DOUGHNUT","BOLT","BOLT","BOLT","SKULL","")); //need to add skills
         teknesDeck.add(new Card("Teknes", "Defender Lineman Controller", "Citizen", "Leader", 1,30,2,"B"
                 ,3,1,6,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","SONIC","BOLT","BOLT","SKULL","8,9,49,50,52"));
+        teknesDeck.add(new Card("Teknes", "Defender Lineman", "Citizen", "Infantry", 1,30,2,"B"
+                ,1,1,5,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","X","SONIC","BOLT","BOLT","SKULL","1,9,51"));
+        teknesDeck.add(new Card("Teknes", "Defender Raeth Sevisk", "Citizen", "Leader Character", 2,30,2,"B"
+                ,3,3,7,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","BOLT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "Addanii Brood Warrior", "Addanii", "Infantry", 2,40,1,"B"
+                ,3,1,5,7,"ARROW","ARROW","DOUGHNUT","DOUGHNUT","SONIC","BOLT","BOLT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "Addanii Brood Masterminds", "Addanii", "Leader", 1,40,1,"B"
+                ,3,1,6,7,"ARROW","ARROW","ARROW","SONIC","SONIC","SONIC","BOLT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "Timoshkin", "Addanii", "Leader Character", 2,40,1,"B"
+                ,5,1,5,7,"SONIC","SONIC","ARROW","ARROW","DOUGHNUT","DOUGHNUT","DOUGHNUT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "Sorik The Unfinished", null, "Specialist Character", 2,40,2,"A"
+                ,2,2,6,6,"STAR","STAR","STAR","DOUGHNUT","DOUGHNUT","DOUGHNUT","BOLT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "The Mighty Taur", null, "Specialist Character", 2,40,4,"A"
+                ,3,2,6,6,"SONIC","SONIC","SONIC","ARROW","ARROW","DOUGHNUT","DOUGHNUT","BOLT","BOLT","SKULL","")); //need to add skills
+        teknesDeck.add(new Card("Teknes", "Lineman Boris", null, "Specialist Character", 2,40,2,"B"
+                ,2,2,6,8,"SONIC","SONIC","SONIC","DOUGHNUT","DOUGHNUT","DOUGHNUT","DOUGHNUT","BOLT","BOLT","SKULL","")); //need to add skills
 
         hadrossDeck.add(new Card("Hadross", "Placeholder", "Citizen", "Leader", 1,30,2,"B"
                 ,3,1,6,6,"DOUGHNUT","DOUGHNUT","DOUGHNUT","DOUGHNUT","X","X","SONIC","BOLT","BOLT","SKULL","8,9,49,50,52"));
