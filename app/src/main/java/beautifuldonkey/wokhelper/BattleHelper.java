6e74a470bd086b2e2f498a9c42e0cba7b7fe6522
beautifuldonkey <beautifuldonkey949@gmail.com>
2015-08-14 23:27:36 -0700

    Adding a test Expandable list view

--- app/src/main/java/beautifuldonkey/wokhelper/BattleHelper.java
+++ app/src/main/java/beautifuldonkey/wokhelper/BattleHelper.java
@@ -4,20 +4,28 @@ import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import beautifuldonkey.wokhelper.Adapters.WokBattleExpandableListAdapter;
 import beautifuldonkey.wokhelper.Data.Card;
 import beautifuldonkey.wokhelper.Data.DeckBuilder;
 import beautifuldonkey.wokhelper.Data.House;
@@ -33,11 +41,40 @@ public class BattleHelper extends ActionBarActivity {
     ArrayList<String> opponentAvailableUnitList;
     ArrayList<String> opponentSelectedUnitList;
 
+    List<String> groupList;
+    List<String> childList;
+    Map<String, List<String>> laptopCollection;
+    ExpandableListView expListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_helper);
         final Context context = this;
+
+        createGroupList();
+
+        createCollection();
+
+        expListView = (ExpandableListView) findViewById(R.id.sectionHolder);
+        final WokBattleExpandableListAdapter expListAdapter = new WokBattleExpandableListAdapter(
+                this, groupList, laptopCollection);
+        expListView.setAdapter(expListAdapter);
+
+        //setGroupIndicatorToRight();
+
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                final String selected = (String) expListAdapter.getChild(
+                        groupPosition, childPosition);
+                Toast.makeText(getBaseContext(), selected, Toast.LENGTH_LONG)
+                        .show();
+
+                return true;
+            }
+        });
 
         List<House> houses = HouseData.getHouseList();
         String[] houseNames = new String[houses.size()];
@@ -143,6 +180,72 @@ public class BattleHelper extends ActionBarActivity {
 
             }
         });
+    }
+
+    private void createGroupList() {
+        groupList = new ArrayList<String>();
+        groupList.add("HP");
+        groupList.add("Dell");
+        groupList.add("Lenovo");
+        groupList.add("Sony");
+        groupList.add("HCL");
+        groupList.add("Samsung");
+    }
+
+    private void createCollection() {
+        // preparing laptops collection(child)
+        String[] hpModels = { "HP Pavilion G6-2014TX", "ProBook HP 4540",
+                "HP Envy 4-1025TX" };
+        String[] hclModels = { "HCL S2101", "HCL L2102", "HCL V2002" };
+        String[] lenovoModels = { "IdeaPad Z Series", "Essential G Series",
+                "ThinkPad X Series", "Ideapad Z Series" };
+        String[] sonyModels = { "VAIO E Series", "VAIO Z Series",
+                "VAIO S Series", "VAIO YB Series" };
+        String[] dellModels = { "Inspiron", "Vostro", "XPS" };
+        String[] samsungModels = { "NP Series", "Series 5", "SF Series" };
+
+        laptopCollection = new LinkedHashMap<String, List<String>>();
+
+        for (String laptop : groupList) {
+            if (laptop.equals("HP")) {
+                loadChild(hpModels);
+            } else if (laptop.equals("Dell"))
+                loadChild(dellModels);
+            else if (laptop.equals("Sony"))
+                loadChild(sonyModels);
+            else if (laptop.equals("HCL"))
+                loadChild(hclModels);
+            else if (laptop.equals("Samsung"))
+                loadChild(samsungModels);
+            else
+                loadChild(lenovoModels);
+
+            laptopCollection.put(laptop, childList);
+        }
+    }
+
+    private void loadChild(String[] laptopModels) {
+        childList = new ArrayList<String>();
+        for (String model : laptopModels)
+            childList.add(model);
+    }
+
+    private void setGroupIndicatorToRight() {
+        /* Get the screen width */
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+
+        expListView.setIndicatorBounds(width - getDipsFromPixel(35), width
+                - getDipsFromPixel(5));
+    }
+
+    // Convert pixel to dip
+    public int getDipsFromPixel(float pixels) {
+        // Get the screen's density scale
+        final float scale = getResources().getDisplayMetrics().density;
+        // Convert the dps to pixels, based on density scale
+        return (int) (pixels * scale + 0.5f);
     }
 
     @Override
